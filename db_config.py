21402731c8713066eaed49980eda9256623d77bf
Sayli <hessabilyas@gmail.com>
2025-04-18 13:35:03 +0200

    ci: add db_config.py

---
+++ db_config.py
@@ -0,0 +1,8 @@
+import os
+
+db_config = {
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASSWORD', 'root'),
+    'database': os.getenv('DB_NAME', 'creativechessai'),
+}
